9554ca4dc1438ebca055d40ba36c8b426ba9b82c
Michael Houmann <michael@michael-h.dk>
2018-11-06 17:56:19 +0100

    Add components Quote and QuoteGrid with scoped style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-
+  <div class="container-fluid">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
-export default {};
+import QuoteGrid from "./components/QuoteGrid";
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        "It's only after we've lost everything that we're free to do anything."
+      ],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    "app-quote-grid": QuoteGrid
+  }
+};
 </script>
 
 <style>
-.quote {
-  font-family: "Caveat", cursive;
-}
 </style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes" :key="quote">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+
+export default {
+  props: ["quotes"],
+  components: {
+    "app-quote": Quote
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
+
